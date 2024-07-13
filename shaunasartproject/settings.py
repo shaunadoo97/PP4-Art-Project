5884c388b7cb742c9f3003f2ef8b2f369ea47bde
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-07-13 17:03:59 +0000

    Trying again with heroku database

--- shaunasartproject/settings.py
+++ shaunasartproject/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'shaunasartproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-#DATABASES = {
+# DATABASES = {
 #   'default': {
 #       'ENGINE': 'django.db.backends.sqlite3',
 #       'NAME': BASE_DIR / 'db.sqlite3',
